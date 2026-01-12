a67673d018ee4eea5e8520d5c4888c1e575d6b66
DRS-53 <>
2026-01-12 13:47:13 +0000

    Arbitrary Arguments

---
+++ arbitrary_arguments/arbitrary_arguments/main.py
@@ -0,0 +1,21 @@
+def calculate_total(*prices):
+    
+    if not prices:  return "Your cart is empty."
+    
+    total = 0
+    
+    for price in prices:
+        total += price 
+
+    if total >= 200:
+        total *= 0.8  # 20% discount
+    elif total >= 100:
+        total *= 0.9 # 10% discount
+
+    return f"Final total: ${total:.2f}"
+
+# Testing the result
+print(calculate_total(30, 20, 50))
+print(calculate_total(100, 50, 80))
+print(calculate_total(150, 100))
+print(calculate_total())
